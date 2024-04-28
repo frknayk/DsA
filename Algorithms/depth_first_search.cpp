cb2cec4815fcba059c00a37e33d56ca8de4bac1b
Furkan <furkanayik@outlook.com>
2024-04-28 19:41:35 +0300

    dfs added

--- Algorithms/depth_first_search.cpp
+++ Algorithms/depth_first_search.cpp
@@ -1,96 +1,115 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
+#include <vector>
+
 using namespace std;
 
-const int MAX_NODES = 6;
-
-/*
-DFS Algorithm
-- Step 1: Insert the root node or starting node of a tree or a graph in the stack.
-- Step 2: Pop the top item from the stack and add it to the visited list.
-- Step 3: Find all the adjacent nodes of the node marked visited and 
-add the ones that are not yet visited, to the stack.
-- Step 4: Repeat steps 2 and 3 until the stack is empty.
-*/
-
-
-// class DFS{
-
-
-
-// };
-
-
-// Structure to represent a stack
-struct Stack {
-    int items[MAX_NODES];
-    int top;
-
-    Stack() {
-        top = -1;
-    }
-
-    void push(int item) {
-        if (top == MAX_NODES - 1) {
-            std::cout << "Stack Overflow!\n";
-            return;
-        }
-        items[++top] = item;
-    }
-
-    int pop() {
-        if (top == -1) {
-            std::cout << "Stack Underflow!\n";
-            return -1; // Return an invalid value
-        }
-        return items[top--];
-    }
-
-    bool isEmpty() {
-        return (top == -1);
-    }
-};
-
-struct Edge{
-    int source;
-    int destination;
-};
-
-// Implementation of DFS without Stack() since recursion already using the 'stack' 
-void depthFirstSearch(
-    int graph[MAX_NODES][MAX_NODES], int start_node, bool visited[MAX_NODES], int num_nodes) {
-    // Make first node visited
-    visited[start_node] = true;
-    std::cout << start_node << " ";
-    // Visit all adjacent nodes of the current node
-    for(int i=0; i<num_nodes;i++){
-        if (!visited[i] && graph[start_node][i]){
-            depthFirstSearch(graph, i, visited, num_nodes);
-        }
-    }
+struct Node {
+    int data;
+    vector<int> adjacentList;
+    Node(int data_): data(data_){}
 };
 
 
-int main() {
-    // Define the graph as pairs of nodes (edges)
-    Edge edges[] = {
-        {0, 1}, {0, 2},
-        {1, 0}, {1, 3}, {1, 4},
-        {2, 0}, {2, 4},
-        {3, 1}, {3, 4}, {3, 5},
-        {4, 1}, {4, 2}, {4, 5},
-        {5, 3}, {5, 4}
-    };
-    // Initialize adjacency matrix with zeros
-    int graph[MAX_NODES][MAX_NODES] = {};
-    // Populate the adjacency matrix based on the edges
-    for (const auto& edge: edges){
-        graph[edge.source][edge.destination] = 1;
-        graph[edge.destination][edge.source] = 1;
+class Graph{
+private:
+    vector<Node> vertexList;
+
+public:
+    Graph(int num_vertices){
+        vertexList.resize(num_vertices, Node(-1));
     }
-    // Array to track visited nodes during DFS
-    bool visited[MAX_NODES] = {false};
-    // Start DFS
-    std::cout<<"Depth First Search starting from node 0: ";
-    depthFirstSearch(graph, 0, visited, MAX_NODES);
+
+    void addEdge(int src, int dest){
+        vertexList[src].adjacentList.push_back(dest);
+        vertexList[dest].adjacentList.push_back(src);
+    }
+
+    void DFS(int start_vertex){
+        // Record visited nodes(vertices)
+        vector<bool> visited(vertexList.size(), false);
+        
+        // Use stack data-structure for DFS
+        stack<int> stack;
+
+        // Start with starter vertex
+        stack.push(start_vertex);
+        // printStack(stack);
+
+        // DFS Traversal
+        while(!stack.empty()){
+            // Get most recent vertex
+            int currentVertex = stack.top();
+            stack.pop();
+
+            if(!visited[currentVertex]){
+                cout<< "\n\n===========vertex: " << currentVertex<<"  ===========\n";
+            
+                // Make unvisited current vertex as 'visited'
+                visited[currentVertex] = true;
+
+                // Add all adjacent nodes to stack
+                int numAdjacents = vertexList[currentVertex].adjacentList.size(); 
+                for(int adj=0; adj<numAdjacents; adj++){
+                    int adjacentNode = vertexList[currentVertex].adjacentList[adj];
+
+                    if(!visited[adjacentNode] and !isExistStack(adjacentNode, stack)){
+                        stack.push(adjacentNode);
+                        cout<<"\n++ pushing ("<<adjacentNode<<") to the stack.";
+                    }
+                }
+                printStack(stack);
+            }
+        }
+    }
+
+    // Function to print the adjacency list representation of the graph
+    void printGraph() {
+        for (size_t i = 0; i < vertexList.size(); ++i) {
+            cout << "Adjacency list of vertex " << i << ":\n";
+            for (int nodeIndex : vertexList[i].adjacentNodes) {
+                cout << "-> " << nodeIndex << " ";
+            }
+            cout << endl;
+        }
+    }
+    
+    void printStack(stack<int> myStack){
+        cout<<"\nStack:(";
+        while(!myStack.empty())
+        {
+            cout << " ";
+            int w = myStack.top();
+            cout << w;
+            myStack.pop();
+        }
+        cout<<" ) ";
+    }
+
+    bool isExistStack(int var, stack<int> myStack){
+        for(int i=0; i<myStack.size(); i++){
+            int ele = myStack.top();
+            if (ele==var) return true;
+            myStack.pop();
+        }
+        return false;
+    }
+
+    
+};
+
+
+
+int main(){
+    Graph graph(5);
+    // Add some edges
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+
+    graph.DFS(0);
     return 0;
 }
